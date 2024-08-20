74a8962951dc8ddc9d0b1905acb5835bd233fedd
Lev <lafitel31@gmail.com>
2024-08-20 16:32:15 +1100

    add routers include, lifespan, name==main

---
+++ fastapi_course/main.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from fastapi import FastAPI
+import uvicorn
+from contextlib import asynccontextmanager
+
+from core.config import settings
+from api import router as api_router
+from core.models import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #startup
+    yield
+    #shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(
+    title="Trading App",
+    lifespan=lifespan,
+)
+
+main_app.include_router(
+    api_router,
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
