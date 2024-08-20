8ed8e06e3fb31e7803153471d72577ee538753d9
Lev <lafitel31@gmail.com>
2024-08-20 16:33:31 +1100

    add runconfig , apiprefix , databaseconfig, settings

---
+++ fastapi_course/core/config.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="FASTAPI__"
+    )
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DataBaseConfig
+
+settings = Settings() # type: ignore
