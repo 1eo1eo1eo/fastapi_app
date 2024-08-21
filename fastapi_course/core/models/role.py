0f12a48be457e4f564dcd4314e52cd35e45f065e
Lev <lafitel31@gmail.com>
2024-08-21 13:12:11 +1100

    role model and user model

--- fastapi_course/core/models/role.py
+++ fastapi_course/core/models/role.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Mapped
 
 
-class Roles(Base):
+class Role(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     permissions: Mapped[JSON] = mapped_column(type_=JSON)
---
+++ fastapi_course/core/models/user.py
@@ -0,0 +1,19 @@
+from core.models import role
+from .base import Base
+
+from datetime import datetime, timezone
+from sqlalchemy import TIMESTAMP, ForeignKey
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Mapped
+
+
+class User(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    registered_at: Mapped[TIMESTAMP] = mapped_column(type_=TIMESTAMP, default=datetime.now)
+    role_id: Mapped[int] = mapped_column(ForeignKey("role.id"))
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(default=False, nullable=False)
