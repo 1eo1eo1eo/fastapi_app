d7af193b06953492cb735fe2012475689b55494d
Lev <lafitel31@gmail.com>
2024-08-22 14:55:02 +1100

    operation table add

--- fastapi_course/core/models/__init__.py
+++ fastapi_course/core/models/__init__.py
@@ -3,8 +3,10 @@ __all__ = (
     "Base",
     "Role",
     "User",
+    "Operation",
 )
 
 from .db_helper import db_helper
 from .base import Base
 from auth.models import Role, User
+from operations.models import Operation
--- fastapi_course/operations/models.py
+++ fastapi_course/operations/models.py
@@ -1,2 +1,14 @@
 from core.models import Base
 
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import TIMESTAMP
+
+
+class Operation(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    quantity: Mapped[str] = mapped_column()
+    figi: Mapped[str] = mapped_column()
+    instrument_type: Mapped[str] = mapped_column(nullable=False)
+    date: Mapped[TIMESTAMP] = mapped_column(type_=TIMESTAMP)
+    type: Mapped[str] = mapped_column()
